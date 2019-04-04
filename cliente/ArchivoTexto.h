cd5756345f0ed7fb54cf92d262c6481744a0431b
Bojorge <duendenener@gmail.com>
2019-04-03 23:35:24 -0600

    Nuevas clases para serializar a binario y crea (escribir y leer) docs
    (escribir y leer)

---
+++ cliente/ArchivoTexto.h
@@ -0,0 +1,14 @@
+//
+// Created by manuel on 03/04/19.
+//
+
+#ifndef CLIENTE_ARCHIVOTEXTO_H
+#define CLIENTE_ARCHIVOTEXTO_H
+
+
+class ArchivoTexto {
+
+};
+
+
+#endif //CLIENTE_ARCHIVOTEXTO_H
---
+++ cliente/Serial.h
@@ -0,0 +1,14 @@
+//
+// Created by manuel on 03/04/19.
+//
+
+#ifndef CLIENTE_SERIAL_H
+#define CLIENTE_SERIAL_H
+
+
+class Serial {
+
+};
+
+
+#endif //CLIENTE_SERIAL_H
