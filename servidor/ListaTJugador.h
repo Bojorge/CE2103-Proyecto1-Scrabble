e60d673f0412bc06041194496ea4d02b48080093
Bojorge <duendenener@gmail.com>
2019-04-10 16:24:38 -0600

    se une la logica al server

---
+++ servidor/ListaTJugador.h
@@ -0,0 +1,14 @@
+//
+// Created by manuel on 10/04/19.
+//
+
+#ifndef SERVIDOR_LISTATJUGADOR_H
+#define SERVIDOR_LISTATJUGADOR_H
+
+
+class ListaTJugador {
+
+};
+
+
+#endif //SERVIDOR_LISTATJUGADOR_H
